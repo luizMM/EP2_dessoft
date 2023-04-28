56b97f8278ad1bd03155c397731a17b3c1b0fe97
Pedro da Costa Alves <pedroca10@al.insper.edu.br>
2023-04-28 10:26:56 -0300

    Função Preenche Frota

---
+++ Preenche Frota.py
@@ -0,0 +1,19 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    return posicoes
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
+    return frota
+
