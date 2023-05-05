3ed375723538223eef2212d23ce391bd29e3accd
luizMM <luizmmb1@al.insper.edu.br>
2023-05-05 12:36:31 -0300

    funcao posicao_valida incompleta

--- define_posições.py
+++ define_posições.py
@@ -1,9 +1,19 @@
-def define_posicoes(linha, coluna, orientacao, tamanho):
-    posicoes = []
-    if orientacao == "vertical":
-        for i in range(tamanho):
-            posicoes.append([linha + i, coluna])
-    elif orientacao == "horizontal":
-        for i in range(tamanho):
-            posicoes.append([linha, coluna + i])
-    return posicoes
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    if frota == {}:
+        return False
+    
+    for navios in frota.values():
+        for posicoes in navios:
+            posicao = []
+            if orientacao == "vertical":
+                for i in range(tamanho):
+                    posicao.append([linha + i, coluna])
+                    if [linha, coluna] in posicoes:
+                        return False
+                return True
+            elif orientacao == "horizontal":
+                for i in range(tamanho):
+                    posicao.append([linha, coluna + i])
+                    if [linha, coluna] in posicoes:
+                        return False
+                return True
